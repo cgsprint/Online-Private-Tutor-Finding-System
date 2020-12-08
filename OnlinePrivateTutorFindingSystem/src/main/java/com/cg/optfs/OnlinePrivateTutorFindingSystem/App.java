921e3be81813995b276c1ac966c9da82f854a115
KokanSpecial <tejashatle2@gmail.com>
2020-12-08 21:19:07 +0530

    First commit

---
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/OnlinePrivateTutorFindingSystem/App.java
@@ -0,0 +1,35 @@
+package com.cg.optfs.OnlinePrivateTutorFindingSystem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.cg.optfs.entity.Admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Admin ad = new Admin();
+    	Admin ad2 = new Admin();
+    	
+    	ad2.setName("tejas");
+    	ad2.setName("tejas");
+    	ad.setName("yash");
+    	ad.setName("yash");
+    	
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA-PU");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	em.getTransaction().begin();
+    	em.persist(ad);
+    	em.persist(ad2);
+    	em.getTransaction().commit();
+    	
+    	System.out.println(ad);
+    }
+}
---
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/dao/AdminDAOImpl.java
@@ -0,0 +1,5 @@
+package com.cg.optfs.dao;
+
+public class AdminDAOImpl {
+
+}
