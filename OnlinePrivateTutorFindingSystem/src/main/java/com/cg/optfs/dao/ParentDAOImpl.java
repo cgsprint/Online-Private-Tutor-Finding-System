2c386045df1d2aab969404503dd3970592c632c9
Shrutu <Shrutu@DESKTOP-AVLBG47>
2020-12-09 17:24:40 +0530

    p

---
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/dao/ParentDAOImpl.java
@@ -0,0 +1,5 @@
+package com.cg.optfs.dao;
+
+public class ParentDAOImpl implements ParentDAO {
+	
+}
--- OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/service/ParentServiceImpl.java
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/service/ParentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.cg.optfs.service;
 
+import com.cg.optfs.dao.ParentDAO;
+import com.cg.optfs.dao.ParentDAOImpl;
 import com.cg.optfs.entity.Parent;
 
 public class ParentServiceImpl implements ParentService {
-
+	ParentDAO pdao;
 	@Override
 	public Parent addParent(Parent parent) {
 		// TODO Auto-generated method stub
+		pdao=new ParentDAOImpl();
 		return null;
 	}
 
