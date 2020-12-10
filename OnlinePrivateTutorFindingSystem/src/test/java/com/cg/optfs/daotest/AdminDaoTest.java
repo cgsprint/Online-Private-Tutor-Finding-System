e235b8105e4cd5ac53e68c92dfd5ca2be2cb1dc0
Manoj <Manoj@LAPTOP-LAI7R7UB.mshome.net>
2020-12-11 00:35:19 +0530

    AdminDAO Junit test is added

--- OnlinePrivateTutorFindingSystem/src/test/java/com/cg/optfs/daotest/AdminDaoTest.java
+++ OnlinePrivateTutorFindingSystem/src/test/java/com/cg/optfs/daotest/AdminDaoTest.java
@@ -1,8 +1,42 @@
 package com.cg.optfs.daotest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import com.cg.optfs.dao.AdminDAO;
+import com.cg.optfs.dao.AdminDAOImpl;
+import com.cg.optfs.dao.TutorDAOImpl;
+import com.cg.optfs.entity.Admin;
+import com.cg.optfs.entity.Tutor;
 
 public class AdminDaoTest {
    AdminDAO adao;
+  @Before
+   public void init(){
+  	 adao= new AdminDAOImpl();
+   }
+   @Test
+  public void addTutor() {
+   	Tutor tutor = new Tutor();
+		tutor.setName("Abc");
+		tutor.setSubject("xyz");
+		tutor.setPhonenumber("7040760465");
+		tutor.setAddress("Bhiwandi");
+		tutor.setQualification("Bsc");
+		Tutor tobj = adao.addTutor(tutor);
+		assertEquals("Bsc", tobj.getQualification());
+	}
+   @Test
+   public void loginTest() {
+	   Admin admin=new Admin();
+	   boolean tobj = adao.loginAdmin("man43", "manoj435");
+		assertTrue(tobj);
+   }
+    
+   
+
    
 }
