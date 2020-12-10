550a5d00427491d496f2b5d48975e19738df800e
visha <visha@LAPTOP-7B17OPSP>
2020-12-10 12:04:37 +0530

    feedback

--- OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/entity/Feedback.java
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/entity/Feedback.java
@@ -2,11 +2,19 @@ package com.cg.optfs.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="Feedback")
 public class Feedback {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="feedbackId",length=4)
+	private int feedbackId;
+	
 	@Column(name="tutorId",length=4)
 	private int tutorId;
 	@Column(name="parentId",length=4)
@@ -14,6 +22,12 @@ public class Feedback {
 	@Column(name="rating",length=3)
 	private int rating;
 	
+	public int getFeedbackId() {
+		return feedbackId;
+	}
+	public void setFeedbackId(int feedbackId) {
+		this.feedbackId = feedbackId;
+	}
 	public int getTutorId() {
 		return tutorId;
 	}
