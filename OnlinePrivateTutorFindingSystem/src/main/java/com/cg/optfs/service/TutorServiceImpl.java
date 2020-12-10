4859293c3714f3554586216cbc3c9e1a7c6720bb
Manoj <Manoj@LAPTOP-LAI7R7UB.mshome.net>
2020-12-10 16:13:37 +0530

    TutorService and TutorServceImpl added

--- OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/service/TutorServiceImpl.java
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/service/TutorServiceImpl.java
@@ -2,27 +2,34 @@ package com.cg.optfs.service;
 
 import java.util.List;
 
+import com.cg.optfs.dao.TutorDAO;
+import com.cg.optfs.dao.TutorDAOImpl;
+import com.cg.optfs.entity.BookedTutor;
 import com.cg.optfs.entity.Request;
 import com.cg.optfs.entity.Tutor;
 
 public class TutorServiceImpl implements TutorService{
-
+     TutorDAO tdao;
+     public TutorServiceImpl(){
+ 		tdao=new TutorDAOImpl();
+ 	}
 	@Override
-	public boolean loginTutor() {
+	public List<Request> viewRequest(Request request, int tutorid) {
 		// TODO Auto-generated method stub
-		return false;
+		return tdao.ViewRequest(request,tutorid);
 	}
 
 	@Override
-	public List<Request> viewdemoRequest() {
+	public List<BookedTutor> ViewBookings(BookedTutor bookings, int tutorid) {
 		// TODO Auto-generated method stub
-		return null;
+		return tdao.ViewBookings(bookings,tutorid);
 	}
 
 	@Override
-	public Tutor updateTutor() {
+	public Tutor updateProfile(Tutor tutor) {
 		// TODO Auto-generated method stub
-		return null;
+		return tdao.updateProfile(tutor);
 	}
+
 
 }
