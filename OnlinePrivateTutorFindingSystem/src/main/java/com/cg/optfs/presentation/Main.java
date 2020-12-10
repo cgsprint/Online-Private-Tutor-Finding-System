6fe8b847d93929980e0b14330c5aee3abcf1b739
KokanSpecial <tejashatle2@gmail.com>
2020-12-10 09:08:08 +0530

    Updated ui

--- OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/presentation/Main.java
+++ OnlinePrivateTutorFindingSystem/src/main/java/com/cg/optfs/presentation/Main.java
@@ -20,66 +20,78 @@ public class Main {
 		AdminService as = new AdminServiceImpl();
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("1. Login");
-		System.out.println("2. Register");
-		System.out.println("Enter the choice number:");
-		int choice = sc.nextInt();
 		
-		switch(choice)
+		System.out.println("1. Admin");
+		System.out.println("2. Tutor");
+		System.out.println("3. Parent");
+		System.out.println("Select your role choice number:");
+		
+		int role = sc.nextInt();
+		
+		switch(role)
 		{
-		case 1:
-			LoginInfo li = new LoginInfo();
-			
-			
-			System.out.println("Enter the username:");
-//			li.setUsername(sc.next());
-			String username = sc.next();
-			
-			System.out.println("Enter the password:");
-//			li.setPassword(sc.next());
-			String password = sc.next();
-			
-//			boolean admin = aa.loginAdmin(username, password);
-//			boolean admin = aa.loginAdmin(li);
-			boolean res = as.loginAdmin(username, password);
-			
-//			System.out.println(res);
-			
-			  if(res)
-			  { 
-				  System.out.println("1. Register Tutor");
-			  System.out.println("2. View parents");
-			  System.out.println("Enter the choice number:");
-			  
-			  choice = sc.nextInt(); 
-			  	switch(choice) 
-			  	{ 
-			  		case 1:
-//			  			Tutor tu = new Tutor();
-			  			
-			  			System.out.println("Enter tutor name: ");
-			  			
+			case 1:
+				LoginInfo li = new LoginInfo();
+				
+				System.out.println("Enter the username:");
+//				li.setUsername(sc.next());
+				String username = sc.next();
+				
+				System.out.println("Enter the password:");
+//				li.setPassword(sc.next());
+				String password = sc.next();
+				
+//				boolean admin = aa.loginAdmin(username, password);
+//				boolean admin = aa.loginAdmin(li);
+				boolean res = as.loginAdmin(username, password);
+				
+//				System.out.println(res);
+				
+				if(res)
+				{ 
+					System.out.println("1. Register Tutor");
+					System.out.println("2. View parents");
+					System.out.println("Enter the choice number:");
+					int choice = sc.nextInt(); 
+				  	switch(choice) 
+				  	{ 
+				  		case 1:
+//						  			Tutor tu = new Tutor();
+						  			
+						 	System.out.println("Enter tutor name: ");
+						  	break;
+						case 2:
+						  		List<org.hibernate.annotations.Parent> lp = as.viewParent();
+						  		System.out.println(lp);
+						  			
+						  	}
+						  }
+				else
+				{
+					System.out.println("Invalid credentials..");
+				}
+						
+				break;	
+				
+			case 2:
+				// Code for Tutor
+				
+				System.out.println("1. Login");
+				System.out.println("2. View Demo requests");
+				System.out.println("3. View Bookings");
+				System.out.println("1. Login");
+				break;
+			case 3:
+				// Code for parent
+				break;
+			default: 
+				System.out.println("Invalid choice");
+				break;
 				  
-			  			break;
-			  		case 2:
-			  		
-			  			
-			  			List<org.hibernate.annotations.Parent> lp = as.viewParent();
-			  			
-			  			System.out.println(lp);
-			  			
-			  			
-			  	}
-			  }
-			  else
-			  {
-				  System.out.println("Invalid credentials..");
-			  }
-			 
+		}
+		
 			
-			
-			break;
 		}
 	}
 
-}
+
